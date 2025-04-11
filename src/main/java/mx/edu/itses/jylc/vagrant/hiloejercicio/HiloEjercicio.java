e83cdc2e9b5cd488945abce6b0bece48379e35f9
Yael Lopez <l.locj050605@itses.edu.mx>
2025-04-11 09:14:36 -0600

    Se sube avance

---
+++ src/main/java/mx/edu/itses/jylc/vagrant/hiloejercicio/HiloEjercicio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.edu.itses.jylc.vagrant.hiloejercicio;
+
+/**
+ *
+ * @author josue
+ */
+public class HiloEjercicio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
